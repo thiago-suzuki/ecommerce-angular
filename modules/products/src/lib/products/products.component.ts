31959cc6aa0381853a66a3041a72313863f9210c
Thiago Suzuki <thiagoikenaga@gmail.com>
2024-01-03 23:24:47 -0300

    feat: add shared-ui library

--- modules/products/src/lib/products/products.component.ts
+++ modules/products/src/lib/products/products.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedUiComponent } from '@ecommerce-angular/shared-ui';
 
 @Component({
   selector: 'ecommerce-angular-products',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SharedUiComponent],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css',
 })
